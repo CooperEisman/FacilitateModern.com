79aca9490b610aec933a3db5e9f33a0cae86486e
Cooper Eisman <76635972+CooperEisman@users.noreply.github.com>
2023-04-01 06:31:41 -0600

    Scroll Fade Away Implemented with Dynamic Backround

--- script.js
+++ script.js
@@ -7,14 +7,9 @@ expandButton.addEventListener('click', function() {
 });
 
 
-
-
-
-
 const textArray = [  "Businesses",  "Websites",  "Graphics",  "Workflows"];
 
 const textContainer = document.getElementById("animation-text");
-const cursor = document.getElementById("animation-cursor");
 
 let currentTextIndex = 0;
 
@@ -35,9 +30,6 @@ function typeText() {
 }
 
 function deleteText() {
-    const currentText = textArray[currentTextIndex];
-    const textLength = currentText.length;
-
     const intervalId = setInterval(() => {
         textContainer.textContent = textContainer.textContent.slice(0, -1);
 
@@ -55,71 +47,72 @@ function deleteText() {
 typeText();
 
 // Listen for scroll events and add class to body if scrolled
-window.addEventListener("scroll", function () {
-    var scrollPos = window.scrollY;
-    var particles = document.getElementById("particles-js");
-    var content = document.querySelector(".content");
-    var opacity = 1 - scrollPos / 800;
-    particles.style.opacity = opacity;
-    content.style.opacity = opacity;
+const scrollDown = document.querySelector('.scroll-down');
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 100) {
+        scrollDown.classList.add('visible');
+    } else {
+        scrollDown.classList.remove('visible');
+    }
 });
 
+// Definitions for Particles
 particlesJS("particles-js", {
     "particles": {
-    "number": {
-        "value": 300,
+        "number": {
+            "value": 507,
             "density": {
-            "enable": true,
+                "enable": true,
                 "value_area": 1104.8066982851817
-        }
-    },
-    "color": {
-        "value": "#ffffff"
-    },
-    "shape": {
-        "type": "circle",
+            }
+        },
+        "color": {
+            "value": "#ffffff"
+        },
+        "shape": {
+            "type": "circle",
             "stroke": {
-            "width": 0,
+                "width": 0,
                 "color": "#ffffff"
-        },
-        "polygon": {
-            "nb_sides": 3
-        },
-        "image": {
-            "src": "img/github.svg",
+            },
+            "polygon": {
+                "nb_sides": 3
+            },
+            "image": {
+                "src": "img/github.svg",
                 "width": 100,
                 "height": 100
-        }
-    },
-    "opacity": {
-        "value": 0.5287289198936227,
+            }
+        },
+        "opacity": {
+            "value": 0.5287289198936227,
             "random": false,
             "anim": {
-            "enable": false,
+                "enable": false,
                 "speed": 1,
                 "opacity_min": 0.1,
                 "sync": false
-        }
-    },
-    "size": {
-        "value": 0,
+            }
+        },
+        "size": {
+            "value": 0,
             "random": true,
             "anim": {
-            "enable": false,
+                "enable": false,
                 "speed": 0,
                 "size_min": 0,
                 "sync": false
-        }
-    },
-    "line_linked": {
-        "enable": true,
+            }
+        },
+        "line_linked": {
+            "enable": true,
             "distance": 150,
             "color": "#ffffff",
             "opacity": 0.4,
             "width": 1
-    },
-    "move": {
-        "enable": true,
+        },
+        "move": {
+            "enable": true,
             "speed": 5,
             "direction": "none",
             "random": false,
@@ -127,55 +120,59 @@ particlesJS("particles-js", {
             "out_mode": "out",
             "bounce": false,
             "attract": {
-            "enable": false,
+                "enable": false,
                 "rotateX": 946.9771699587272,
                 "rotateY": 1200
-        }
-    }
-},
-    "interactivity": {
-    "detect_on": "canvas",
-        "events": {
-        "onhover": {
-            "enable": true,
-                "mode": "repulse"
-        },
-        "onclick": {
-            "enable": true,
-                "mode": "push"
-        },
-        "resize": true
-    },
-    "modes": {
-        "grab": {
-            "distance": 400,
-                "line_linked": {
-                "opacity": 1
             }
+        }
+    },
+    "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+            "onhover": {
+                "enable": true,
+                "mode": "repulse"
+            },
+            "onclick": {
+                "enable": true,
+                "mode": "push"
+            },
+            "resize": true
         },
-        "bubble": {
-            "distance": 400,
+        "modes": {
+            "grab": {
+                "distance": 400,
+                "line_linked": {
+                    "opacity": 1
+                }
+            },
+            "bubble": {
+                "distance": 400,
                 "size": 40,
                 "duration": 2,
                 "opacity": 8,
                 "speed": 3
-        },
-        "repulse": {
-            "distance": 56.84540486109416,
+            },
+            "repulse": {
+                "distance": 56.84540486109416,
                 "duration": 0.4
-        },
-        "push": {
-            "particles_nb": 4
-        },
-        "remove": {
-            "particles_nb": 2
+            },
+            "push": {
+                "particles_nb": 4
+            },
+            "remove": {
+                "particles_nb": 2
+            }
         }
-    }
-},
+    },
     "retina_detect": true
 });
 
-
-
-
-
+window.addEventListener("scroll", function () {
+    let scrollPos = window.scrollY;
+    let particles = document.getElementById("particles-js");
+    let content = document.querySelector(".content");
+    let opacity = 1 - scrollPos / 800;
+    particles.style.opacity = opacity;
+    content.style.opacity = opacity;
+});
