d42dcd7bb4898c51d0fc16771852dd510ad3328a
Cooper Eisman <76635972+CooperEisman@users.noreply.github.com>
2023-04-01 04:42:22 -0600

    Text Typing

--- script.js
+++ script.js
@@ -6,50 +6,50 @@ expandButton.addEventListener('click', function() {
     sidebar.classList.toggle('expanded');
 });
 
-const TEXT_ROTATE_DELAY = 2000;
-const TEXT_ROTATE_ITEMS = [
-    "This is the first piece of text.",
-    "Here is the second piece of text.",
-    "This is the third text item in the rotation.",
-    "Number four in the rotation of text items.",
-    "And this is the fifth and final piece of text in the rotation."
-];
-let textRotateIndex = 0;
-let textRotateTimeout;
 
-function rotateText() {
-    document.querySelector('.text-rotate').textContent = '';
-    let currentText = TEXT_ROTATE_ITEMS[textRotateIndex];
+
+
+
+
+const textArray = [  "Businesses",  "Websites",  "Graphics",  "Workflows"];
+
+const textContainer = document.getElementById("animation-text");
+const cursor = document.getElementById("animation-cursor");
+
+let currentTextIndex = 0;
+
+function typeText() {
+    const currentText = textArray[currentTextIndex];
+    const textLength = currentText.length;
     let i = 0;
-    textRotateTimeout = setInterval(() => {
-        if (i < currentText.length) {
-            document.querySelector('.text-rotate').textContent += currentText.charAt(i);
-            i++;
-        } else {
-            clearInterval(textRotateTimeout);
-            textRotateTimeout = setTimeout(() => {
-                deleteText();
-            }, TEXT_ROTATE_DELAY);
+
+    const intervalId = setInterval(() => {
+        textContainer.textContent += currentText[i];
+        i++;
+
+        if (i === textLength) {
+            clearInterval(intervalId);
+            setTimeout(deleteText, 1000);
         }
-    }, 50);
-    textRotateIndex = (textRotateIndex + 1) % TEXT_ROTATE_ITEMS.length;
+    }, 120);
 }
 
 function deleteText() {
-    let currentText = document.querySelector('.text-rotate').textContent;
-    let i = currentText.length - 1;
-    textRotateTimeout = setInterval(() => {
-        if (i >= 0) {
-            let newText = currentText.slice(0, i);
-            document.querySelector('.text-rotate').textContent = newText;
-            i--;
-        } else {
-            clearInterval(textRotateTimeout);
-            textRotateTimeout = setTimeout(() => {
-                rotateText();
-            }, TEXT_ROTATE_DELAY);
+    const currentText = textArray[currentTextIndex];
+    const textLength = currentText.length;
+
+    const intervalId = setInterval(() => {
+        textContainer.textContent = textContainer.textContent.slice(0, -1);
+
+        if (textContainer.textContent === "") {
+            clearInterval(intervalId);
+            currentTextIndex++;
+            if (currentTextIndex === textArray.length) {
+                currentTextIndex = 0;
+            }
+            setTimeout(typeText, 1200);
         }
-    }, 30);
+    }, 100);
 }
 
-rotateText();
+typeText();
